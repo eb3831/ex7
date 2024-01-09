9ec6746142dbb4f876eb3916cf15aa598f1e06ed
eb3831 <eb3831@bs.amalnet.k12.il>
2024-01-09 09:40:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ex7/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.ex7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton ibtn;
+
+    ImageView iv;
+
+    int rndNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ibtn = findViewById(R.id.ibtn);
+        iv = findViewById(R.id.iv);
+
+
+    }
+
+
+
+    public void clicked(View view) {
+        Random rnd = new Random();
+        rndNum= rnd.nextInt(3)+1;
+        if (rndNum==1){
+            iv.setImageResource(R.drawable.minion1);
+            ibtn.setImageResource(R.drawable.num1);
+        }
+        if (rndNum==2){
+            iv.setImageResource(R.drawable.minion2);
+            ibtn.setImageResource(R.drawable.num2);
+        }
+        if (rndNum==3){
+            iv.setImageResource(R.drawable.minion3);
+            ibtn.setImageResource(R.drawable.num3);
+        }
+    }
+}
